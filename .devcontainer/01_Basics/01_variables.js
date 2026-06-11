a1f5f35f3c53dbeae859b889a1853283e75f090c
Sakshant Nagmote <sakshantnagmote01@gmail.com>
2026-06-11 05:40:32 +0000

    datatypes

---
+++ .devcontainer/01_Basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 12548
+let accountEmail = "sakky12@.gmail.com"
+var accountPass = "121212"
+accountCity = "Nagpur"
+let accountState;
+
+// accountId = 2 / NOT ALLOWED 
+
+accountEmail = "sa@.com"
+accountPass = "2121256"
+accountCity = "Wardha"
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPass);
+
+//  INSTED OF THIS WE CAN ALSO WRITE 
+
+
+console.log([accountEmail, accountId, accountPass, accountState])
---
+++ .devcontainer/01_Basics/02_datatypes.js
@@ -0,0 +1,18 @@
+let name = "Sakky"
+let age = "20"
+let isloggedIn = true
+
+
+
+// // DATATYPES  
+// number => 
+// string => "" 
+// boolean => True/false
+// symbol => uniqness
+// 
+
+console.log(typeof null); // output = object
+console.log(typeof undefined); // output = undefined
+
+
+
