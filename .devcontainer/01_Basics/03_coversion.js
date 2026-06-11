eb1622701bcd6e6df05f8ba11a923c1dad1d2a28
Sakshant Nagmote <sakshantnagmote01@gmail.com>
2026-06-11 07:16:13 +0000

    Datatypes summery.js

---
+++ .devcontainer/01_Basics/03_coversion.js
@@ -0,0 +1,34 @@
+let score = 33 
+
+console.log(typeof score);
+console.log(typeof (score);   
+// BOTH ARE SAME WE CAN WRITE IN METHOD ALSO 
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abs" => Nan 
+// true => 1
+// false => 0
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+// /==============================/====================================/
+
+let str1 = "Hello"
+let str2 = " Sakky"
+let str3 = str1 + str2
+
+console.log(str3) = str1 + str2     // Hello Sakky
+
+
+console.log("1" + 2);   // 12
+console.log(1 + "2") ;   // 12
+console.log("1" + 2 + 2);  // 122
+console.log(1 + 2 + "2");    //32
---
+++ .devcontainer/01_Basics/Datatypes summery.js
@@ -0,0 +1,19 @@
+// Primitive DATA TYPES :- 7 types - string, number, boolean, undefined, null, Bigint, symbol 
+// same we see in previous video
+
+
+// Non primitive :-     Array , objects, functions 
+
+const heros = ["batman", "shaktiman", "thor"]    //array
+
+
+let myObj {
+    name: "sakky",
+    age: 21
+}                                                 // object (inside {} this)
+
+
+const myFunction = function(){
+     console.log("Hello Workld");
+}   
+
