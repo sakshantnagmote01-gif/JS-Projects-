d6f76b2dc1a79d9546b87ca6eaabc96bd6fb92a9
Sakshant Nagmote <sakshantnagmote01@gmail.com>
2026-06-13 06:26:09 +0000

    04_string.js

---
+++ .devcontainer/01_Basics/04_string.js
@@ -0,0 +1,27 @@
+const name = "sakky"
+const repocount = 50
+
+console.log(name + repocount);
+console.log(`Hello my name is ${namw} and my repocount is ${repocount}`);    // BOTH ARE SAME 
+
+const gameName = new string("sakky")
+console.log(gameName[0]);
+console.log(gameName__proto__);          // IT GIVES THE PROTOTYPE 
+
+console.log(gameName.CharArt(2));
+
+console.log(gameName.index of(N));
+
+console.log(gameName.upperCase());
+
+console.log(gameName.lengh);
+
+
+const newString ="  Sakshant  "
+console.log(newString);
+console.log(newString.trim());       // IT ERASE THE SPACE FROM OUTPUT RESULT
+
+
+// THIS ARE THE PROPRTYT IN WHICH WW CAN ADDRESSE THIS IN VARIOUS ASPECTS 
+
+
